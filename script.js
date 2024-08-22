91ca32c68e0c6f7bef29bfbd0133d5c2afa6866a
bugDje <bugdje@gmail.com>
2024-08-22 23:53:48 +0200

    V1.12

--- script.js
+++ script.js
@@ -1,15 +1,30 @@
-let navbar = document.querySelector("nav");
+let navBar = document.querySelector("nav");
+let menuBar = document.querySelector(".menu");
+let scrollDown = navBar.querySelectorAll("li .scroll-down");
+
 let scrollPrevious = window.scrollY;
 
 window.onscroll = function () {
   let scrollCurrent = window.scrollY;
   if (scrollPrevious > scrollCurrent) {
-    navbar.style.top = "0px";
+    navBar.style.top = "0px";
+    menuBar.style.bottom = "-90px";
   } else {
-    navbar.style.top = "-90px";
+    navBar.style.top = "-90px";
+    menuBar.style.bottom = "0px";
   }
   scrollPrevious = scrollCurrent;
 };
 imgFull.addEventListener("click", () => {
   scroll(0, 100);
+});
+
+menuFooter.addEventListener("click", () => {
+  scroll(0, 0);
+});
+
+scrollDown.forEach(function(user){
+  user.addEventListener("click", ()=>{
+    scroll(0, 100);
+  })
 });
